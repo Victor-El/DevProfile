5fb513658a09825351cc1844714ff563ec5a6ff2
Victor-El <victorelezua@gmail.com>
2020-06-21 20:17:13 +0100

    Initial commit

---
+++ app/src/main/java/me/codeenzyme/devprofile/view/MainActivity.java
@@ -0,0 +1,16 @@
+package me.codeenzyme.devprofile.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.codeenzyme.devprofile.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
